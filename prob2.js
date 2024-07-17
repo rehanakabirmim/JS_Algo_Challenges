58213d8f2021c4983a639052ff3b360733759d33
rehanakabirmim <rehanakabirmim@gmail.com>
2024-07-18 01:25:02 +0600

    prob2 done

---
+++ prob2.js
@@ -0,0 +1,22 @@
+//Q2:
+// Write a function that returns the sum of two numbers.
+// Write a function that returns the sum of all numbers regardless of # of params.
+// exam:For param1 = 1 and param2 = 2, the output should be add(param1, param2) = 3.
+// hints:Arithmetic Operators
+// Rest Operators
+// forEach()
+
+function sum(param1,param2){
+    return param1 + param2;
+}
+console.log(sum(1,3))
+
+function totalSum(...param1){
+    let total = 0;
+    param1.forEach(perElem=>{
+        total = total+perElem;
+    })
+    return total;
+
+}
+console.log(totalSum(1,3,4));
